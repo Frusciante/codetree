1298d589275e7b57e39c9b4cd77b6af05f88ca3f
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-05-23 22:02:26 +0900

    Title: ABC 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250523/ABC 줄 세우기/abc-line-up.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+char arr[26];
+
+void swap(char* c1, char* c2)
+{
+    char tmp = *c1;
+    *c1 = *c2;
+    *c2 = tmp;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    // Please write your code here.
+
+    int cnt = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+                cnt++;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) { cout << arr[i] << ' '; };
+
+
+    cout << endl << cnt;
+
+    return 0;
+}
