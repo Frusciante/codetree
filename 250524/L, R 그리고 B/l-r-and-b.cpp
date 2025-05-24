6257ec62b94ca8828c6076a95768d512bad1de86
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-05-24 22:59:36 +0900

    Title: L, R 그리고 B, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250524/L, R 그리고 B/l-r-and-b.cpp
+++ 250524/L, R 그리고 B/l-r-and-b.cpp
@@ -17,6 +17,9 @@ int main() {
     loc r_loc = {};
     loc b_loc = {};
 
+    int cnt = 0;
+    char order[3] = {};
+
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             cin >> board[i][j];
@@ -25,27 +28,37 @@ int main() {
                 case 'B':
                 b_loc.row = i;
                 b_loc.col = j;
+                order[cnt] = 'B';
+                cnt++;
                 break;
 
                 case 'R':
                 r_loc.row = i;
                 r_loc.col = j;
+                order[cnt] = 'R';
+                cnt++;
                 break;
 
                 case 'L':
                 l_loc.row = i;
                 l_loc.col = j;
+                order[cnt] = 'L';
+                cnt++;
                 break;
             }
         }
     }
 
+    bool r_is_between_b_and_l = (order[1] == 'R');
+
     // Please write your code here.
 
     int min_dist = 100;
 
-    if (b_loc.row == r_loc.row && r_loc.row == l_loc.row ||
+    if ((b_loc.row == r_loc.row && r_loc.row == l_loc.row ||
         b_loc.col == r_loc.col && r_loc.col == l_loc.col)
+        && r_is_between_b_and_l
+        )
     {
         min_dist = abs(b_loc.row - l_loc.row) + abs(b_loc.col - l_loc.col) + 1;
     }
