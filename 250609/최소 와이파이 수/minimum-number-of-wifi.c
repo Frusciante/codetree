ef4c11653d33113675729658a7f0c9e650ba23f3
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-06-09 21:36:20 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250609/최소 와이파이 수/minimum-number-of-wifi.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int n, m;
+int arr[101];
+
+enum
+{
+    NO_WIFI_VACANT, NO_WIFI_LIVING, WIFI_VACANT, WIFI_LIVING
+};
+
+int main() 
+{
+    int temp;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &temp);
+        arr[i] = (temp == 1) ? NO_WIFI_LIVING : NO_WIFI_VACANT;
+    }
+    
+    // Please write your code here.
+
+    int cnt = 0;
+
+    for (int i = m; i < n; i++)
+    {
+        if (arr[i - m] == NO_WIFI_LIVING)
+        {
+            cnt++;
+            for (int j = i - m; j <= i; j++)
+            {
+                arr[j] = (arr[j] == NO_WIFI_LIVING) ? WIFI_LIVING : WIFI_VACANT;
+            }
+        }
+    }
+
+    printf("%d", cnt);
+    
+    return 0;
+}
