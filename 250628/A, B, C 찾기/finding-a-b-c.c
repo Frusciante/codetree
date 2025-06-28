c18b2e620ece3111effabb58b0fc5a43d79bdf5d
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-06-28 22:51:51 +0900

    Title: A, B, C 찾기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250628/A, B, C 찾기/finding-a-b-c.c
+++ 250628/A, B, C 찾기/finding-a-b-c.c
@@ -4,6 +4,13 @@
 
 int arr[NUM_ARR];
 
+int asc(const void* first, const void* second)
+{
+    if (*(int*)first > *(int*)second) { return 1; }
+    if (*(int*)first <*(int*)second) { return -1; }
+    return 0;
+}
+
 int main(void) {
     int i;
     for (i = 0; i < NUM_ARR; i++) {
@@ -13,9 +20,11 @@ int main(void) {
 
     int a, b, c;
 
+    qsort(arr, NUM_ARR, sizeof(int), asc);
+
     a = arr[0];
-    b = arr[3] - a;
-    c = arr[4] - b;
+    b = arr[1];
+    c = arr[6] - b - a;
 
     printf("%d %d %d", a, b, c);
 
