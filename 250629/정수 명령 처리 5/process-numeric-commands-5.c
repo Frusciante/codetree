c549bdb36e446a806a7c9871aa03cee3f7240681
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-06-29 23:05:07 +0900

    Title: 정수 명령 처리 5, Time: 8ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 250629/정수 명령 처리 5/process-numeric-commands-5.c
+++ 250629/정수 명령 처리 5/process-numeric-commands-5.c
@@ -31,13 +31,17 @@ int pop_back(ARRAY* arr)
 {
     if (arr->size <= 0)
     {
-        arr->container = NULL;
-        arr->size = 0;
         return 2;
     }
 
     arr->size--;
-    if (arr->size > 0)
+
+    if (arr->size == 0)
+    {
+        free(arr->container);
+        arr->container = NULL;
+    }
+    else
     {
         int* new_container = (int*)realloc(arr->container, ((arr->size) - 1) * sizeof(int));
         if (new_container == NULL)
@@ -45,12 +49,6 @@ int pop_back(ARRAY* arr)
             return 1;
         }
         arr->container = new_container;
-        
-    }
-    else
-    {
-        free(arr->container);
-        arr->container = NULL;
     }
     
     return 0;
