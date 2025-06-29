e8f3fd90367d0d86deea3df018ca9dc8a8797731
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-06-29 22:56:04 +0900

    Title: 정수 명령 처리 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/정수 명령 처리 5/process-numeric-commands-5.c
+++ 250629/정수 명령 처리 5/process-numeric-commands-5.c
@@ -29,19 +29,30 @@ int push_back(ARRAY* arr, int n)
 
 int pop_back(ARRAY* arr)
 {
-    if (arr->size == 0)
+    if (arr->size <= 0)
     {
+        arr->container = NULL;
+        arr->size = 0;
         return 2;
     }
 
-    int* new_container = (int*)realloc(arr->container, ((arr->size) - 1) * sizeof(int));
-    if (new_container == NULL)
+    if (arr->size > 1)
     {
-        return 1;
+        int* new_container = (int*)realloc(arr->container, ((arr->size) - 1) * sizeof(int));
+        if (new_container == NULL)
+        {
+            return 1;
+        }
+        arr->container = new_container;
+        arr->size--;
     }
-    arr->container = new_container;
-    arr->size--;
-
+    else
+    {
+        arr->size = 0;
+        free(arr->container);
+        arr->container = NULL;
+    }
+    
     return 0;
 }
 
@@ -78,7 +89,7 @@ int main()
         } 
         else if (strcmp(command, "pop_back") == 0) 
         {
-            pop_back(&arr) == 1;
+            pop_back(&arr);
 
         } 
         else if (strcmp(command, "size") == 0) 
@@ -87,7 +98,10 @@ int main()
         }
     }
 
-    free(arr.container);
+    if (arr.container != NULL)
+    {
+        free(arr.container);
+    }
     
     // Please write your code here.
     
