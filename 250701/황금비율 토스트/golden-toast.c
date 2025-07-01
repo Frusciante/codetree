b046787b5e17c4a466abfb996c9ff0f4373d80d1
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-07-01 18:54:24 +0900

    Title: 황금비율 토스트, Time: 19ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250701/황금비율 토스트/golden-toast.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+char s[100005];
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    scanf("%s", s, 100005);
+    char* iter = s + n;
+    char* end = s + n; // end (마지막 원소 + 1)
+    char* begin = s;
+    char char_to_insert;
+    getchar();
+
+    for (int i = 0; i < m; i++) {
+        char command;
+        scanf("%c", &command, 1);
+        getchar();
+        switch (command)
+        {
+        case 'L':
+            if (iter != begin)
+            {
+                iter--;
+            }
+            break;
+        case 'R':
+            if (iter != end)
+            {
+                iter++;
+            }
+            break;
+        case 'P':
+            scanf("%c", &char_to_insert, 1);
+            getchar();
+            ++end;
+            for (char* temp_iter = end; temp_iter != iter; --temp_iter)
+            {
+                *temp_iter = *(temp_iter - 1);
+            }
+            *end = 0;
+            *iter = char_to_insert;
+            ++iter;
+            break;
+        case 'D':
+            for (char* temp_iter = iter; temp_iter != end; ++temp_iter)
+            {
+                *temp_iter = *(temp_iter + 1);
+            }
+            ++iter;
+            break;
+        }
+    }
+
+    printf("%s", s);
+
+    // Please write your code here.
+    return 0;
+}
