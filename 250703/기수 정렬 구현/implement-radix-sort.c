96ffff8628a100cd367742cbdfc205da29b28d91
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-07-03 17:37:22 +0900

    Title: 기수 정렬 구현, Time: 21ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 250703/기수 정렬 구현/implement-radix-sort.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+#define MAX_N 100000
+#define MAX_DIGIT 5
+
+int n;
+int arr[MAX_N];
+
+typedef struct node
+{
+    struct node* next;
+    int data;
+} NODE;
+
+typedef struct int_linked_list
+{
+    NODE* head;
+    NODE* tail;
+} INT_LINKED_LIST;
+
+int push_back(INT_LINKED_LIST* list, int data)
+{
+    if (!list) { return 1; }
+
+    NODE* new_node = (NODE*)malloc(sizeof(NODE));
+    if (!new_node) { return 1; }
+
+    new_node->next = NULL;
+    new_node->data = data;
+
+    if (list->tail) { list->tail->next = new_node; }
+    list->tail = new_node;
+
+    if (!list->head) { list->head = new_node; }
+
+    return 0;
+}
+
+int pop_front(INT_LINKED_LIST* list, int* data)
+{
+    if (!(list && data)) { return 1; }
+    if (!list->head) { return 1; }
+
+    *data = list->head->data;
+
+    NODE* temp = list->head;
+    list->head = list->head->next;
+    free(temp);
+
+    if (!list->head) { list->tail = NULL; }
+
+    return 0;
+}
+
+void radix_sort(INT_LINKED_LIST* list, int* arr, int arr_size)
+{
+    for (int i = MAX_DIGIT; i >= 0; --i)
+    {
+        for (int j = 0; j < arr_size; ++j)
+        {
+            push_back(&(list[arr[j] / (int)pow(10.0, (double)i)]), arr[j]);
+        }
+
+        int cnt = 0;
+        for (int j = 0; j < 10; ++j)
+        {
+            int pop_front_result;
+            while (pop_front(&list[j], &pop_front_result) == 0)
+            {
+                arr[cnt] = pop_front_result;
+                ++cnt;
+            }
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d", &n);
+
+    INT_LINKED_LIST list[10] = { 0, };
+
+    for (int i = 0; i < n; ++i) { scanf("%d", &arr[i]); }
+
+    // Please write your code here.
+    radix_sort(list, arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
