4160b6efa2d052a28b1c5cd18362b07dcfe18335
Frusciante <149743835+Frusciante@users.noreply.github.com>
2025-07-05 17:18:20 +0900

    Title: 병합 정렬 구현, Time: 33ms, Memory: 1MB, Status: Passed - Codetree

--- 250705/병합 정렬 구현/implement-merge-sort.c
+++ 250705/병합 정렬 구현/implement-merge-sort.c
@@ -15,30 +15,12 @@ int merge(int* arr, int low, int mid, int high)
         return 1;
     }
 
-    //printf("low : %d, mid : %d, high : %d\n", low, mid, high);
-
     memcpy(aux_arr1, arr + low, sizeof(int) * (mid - low + 1));
-    //printf("aux_arr1 : ");
-    //for (int i = 0; i < mid - low + 1; i++)
-    //{
-    //    printf("%d ", aux_arr1[i]);
-    //}
-    //puts("");
-
     memcpy(aux_arr2, arr + (mid + 1), sizeof(int) * (high - mid));
-    //printf("aux_arr2 : ");
-    //for (int i = 0; i < high - mid; i++)
-    //{
-    //    printf("%d ", aux_arr2[i]);
-    //}
-    //puts("");
 
     int i = 0;
     int j = 0;
     int k = low;
-
-    char i_end_yet = 0;
-    char j_end_yet = 0;
 
     while (i < mid - low + 1 && j < high - mid)
     {
@@ -74,12 +56,6 @@ int merge(int* arr, int low, int mid, int high)
 
     free(aux_arr1);
     free(aux_arr2);
-
-    //for (int i = 0; i < n; i++)
-    //{
-    //    printf("%d ", arr[i]);
-    //}
-    //puts("");
 
     return 0;
 }
